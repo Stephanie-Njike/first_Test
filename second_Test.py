02dd5a56b63058bb25ce3cad355ff66b25fcd9fa
Stephanie-Njike <stephaniepascale0@yahoo.fr>
2021-09-20 10:30:39 +0200

    My first Test

--- second_Test.py
+++ second_Test.py
@@ -2,6 +2,7 @@
 import unittest
 
 # Solution 1
+print('************** Premiere solution **************')
 def mediane (a,b,c):
     if (a<= c and c<=b) or (b<=c and c<=a):
         median = c
@@ -15,9 +16,12 @@ print(mediane(2,2,2))
 print(mediane(1,2,3))
 print(mediane(8,-3,11))
 
+print('************** Fin de la premiere solution **************')
+
 
 
 # Solution 2
+print('************** Deuxieme solution **************')
 def mediane1 (a,b,c):
     ##a,b,c=3,2,4
     if a>=b and a<=c:
@@ -27,13 +31,15 @@ def mediane1 (a,b,c):
     elif c>=a and c<=b:
         median=c
     return median
-print(mediane1(6,4,5))
+#print(mediane1(6,4,5))
 print(mediane1(2,2,2))
 print(mediane1(1,2,3))
 print(mediane1(8,-3,11))
 
+print('************** Fin de la deuxieme solution **************')
 
 # Solution 3
+print('************** Troisieme solution **************')
 def mediane2 (a,b,c):
     median = a
     if a < b < c and c < b < a :
@@ -45,32 +51,40 @@ print(mediane2(6,4,5))
 print(mediane2(2,2,2))
 print(mediane2(1,2,3))
 print(mediane2(8,-3,11))
+print('************** Fin de la troisieme solution **************')
+
 '''
-class TestMediane(unittest.TestCase):
+print('************** Premiere solution **************')
+class TestMediane(unittest.TestCase):    
     def test_mediane(self):
         self.assertEqual(mediane(2,2,2), 2)
         self.assertEqual(mediane(1,2,3), 2)
         self.assertEqual(mediane(6,4,5), 5)
         self.assertEqual(mediane(8,-3,11), 8)
-
-
+print('************** Fin de la premiere solution **************')
+'''
+print('************** Deuxieme solution **************')
 class TestMediane1(unittest.TestCase):
     def test_mediane1(self):
         self.assertEqual(mediane1(2,2,2), 2)
         self.assertEqual(mediane1(1,2,3), 2)
         self.assertEqual(mediane1(6,4,5), 5)
         self.assertEqual(mediane1(8,-3,11), 8)
-
-
-class TestMediane(unittest.TestCase):
+print('************** Fin de la deuxieme solution **************')
+'''
+print('************** Troisieme solution **************')
+class TestMediane2(unittest.TestCase):
     def test_mediane2(self):
         self.assertEqual(mediane2(2,2,2), 2)
         self.assertEqual(mediane2(1,2,3), 2)
         self.assertEqual(mediane2(6,4,5), 5)
         self.assertEqual(mediane2(8,-3,11), 8)
 
+
+print('************** Fin de la troisieme solution **************')
+'''
 if __name__ == '__main__':
     unittest.main()
-'''
+
 
 
